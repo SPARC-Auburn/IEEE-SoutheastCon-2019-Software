ab14dfc695f39d2688daea51b1aa4a79d2110608
Matthew Castleberry <mattcastle38@gmail.com>
2018-11-16 14:56:55 -0600

    established serial control - arduino side

---
+++ Main-Program-1/Arduino-Serial/SerialForArduino/SerialForArduino.ino
@@ -0,0 +1,48 @@
+const int pinLeft1 = 3;
+const int pinLeft2 = 2;
+const int pinRight2 = 4;
+const int pinRight1 = 5;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // make the pins outputs:
+  pinMode(pinLeft1, OUTPUT);
+  pinMode(pinRight1, OUTPUT);
+  pinMode(pinLeft2, OUTPUT);
+  pinMode(pinRight2, OUTPUT);
+}
+
+void loop() {
+  // if there's any serial available, read it:
+  while (Serial.available() > 0) {
+    int dir1  = Serial.parseInt();
+    int speed1  =  Serial.parseInt();
+    int dir2  =  Serial.parseInt();
+    int speed2  =  Serial.parseInt();
+    if (dir1 == 1){
+      analogWrite(pinLeft2,speed1);
+      digitalWrite(pinLeft1,LOW);
+    }
+    else {
+      analogWrite(pinLeft1,speed1);
+      digitalWrite(pinLeft2,LOW);
+    }
+    if (dir2 == 1){
+      analogWrite(pinRight2,speed2);
+      digitalWrite(pinRight1,LOW);
+    }
+    else {
+      analogWrite(pinRight1,speed2);
+      digitalWrite(pinRight2,LOW);
+    }      
+  }
+ }
+
+
+
+
+
+
+
+
