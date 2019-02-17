8c2b19548164ba7efbc46fb6eaad8fab37e0dfbc
mcberry23 <mtc0030@auburn.edu>
2019-02-17 14:41:54 -0600

    Started merge of Alex's and Joe's programs

---
+++ Main-Program-1/Arduino-Serial/ArduinoSerial.cpp
@@ -0,0 +1,125 @@
+#include "ArduinoSerial.h"
+#include <stdexcept>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <cerrno>
+#include <cstring>		//for strerror()
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+
+const char serialPort::typicalPortName[] = "/dev/ttyACM0";
+
+serialPort::serialPort(const char* portName) {
+  fileHandle = open(portName, O_RDWR | O_NOCTTY | O_NDELAY);
+  if(fileHandle == -1) {
+    throw std::runtime_error(string("Error opening port: ") + strerror(errno));
+  }
+  if(!isatty(fileHandle)) {
+    close(fileHandle);
+    throw std::runtime_error("Port is not a serial device.");
+  }
+
+  cfmakeraw(&config);     //Sets various parameters for non-canonical mode; disables parity
+
+  cfsetospeed (&config, B115200);    //Baud rate
+  cfsetispeed (&config, B115200);
+
+  config.c_cflag     &=  ~CSTOPB;    //One stop bit
+
+  config.c_cflag     |=  CREAD | CLOCAL;
+  config.c_cflag     &=  ~CRTSCTS;           // no flow control
+
+  config.c_cc[VMIN]   =  0;
+  config.c_cc[VTIME]  =  0;
+
+  tcflush(fileHandle, TCIFLUSH);    //clear input buffer
+  if(tcsetattr(fileHandle, TCSANOW, &config) != 0) {
+    close(fileHandle);
+    throw std::runtime_error("Setting attributes failed.");
+  }
+}
+
+void serialPort::write(string text) {
+  ::write(fileHandle, text.c_str(), text.length());
+}
+
+string serialPort::read() {
+  string input;
+  int bytes = available();
+  input.resize(bytes);
+  ::read(fileHandle, const_cast<char*>(input.data()) , bytes);    //The const cast is less than ideal
+  return(input);
+}
+
+int serialPort::available() {
+  int bytes;
+  ioctl(fileHandle, FIONREAD, &bytes);
+  return(bytes);
+}
+
+void serialPort::controlMotors(int motor, int speed) {
+	if(speed1 < -127 || speed1 > 127 || speed2 < -127 || speed1 > 127)
+		throw out_of_range("Motor speed must be between 0 and 127.");
+	char x[2] = {(char)(speed1+127),(char)(speed2+127)};
+	::write(fileHandle,x,2);
+}
+
+void serialPort::turnLeft(int speed){
+  controlMotors(1,speed);
+  controlMotors(2,-speed);
+}
+
+void serialPort::turnRight(int speed){
+  controlMotors(1,-speed);
+  controlMotors(2, speed);
+}
+
+void serialPort::goForward(int speed){
+  controlMotors(1,-speed);
+  controlMotors(2,-speed);
+}
+
+void serialPort::goBackward(int speed){
+  controlMotors(1,speed);
+  controlMotors(2,speed);
+}
+
+void serialPort::raiseGate(int speed){
+  controlMotors(3,speed);
+  controlMotors(4,speed);
+}
+
+void serialPort::holdGate(int speed){
+  controlMotors(3,0);
+  controlMotors(4,0);
+}
+
+void serialPort::lowerGate(int speed){
+  controlMotors(3,-speed);
+  controlMotors(4,-speed);
+}
+
+void serialPort::rotateForward(int speed){
+  controlMotors(5,speed);
+}
+
+void serialPort::rotateBackward(int speed){
+  controlMotors(5,-speed);
+}
+
+void serialPort::stopBar(int speed){
+  controlMotors(5,0);
+}
+
+void serialPort::stopMotors(){
+  controlMotors(1,0);
+  controlMotors(2,0);
+}
+
+serialPort::~serialPort() {
+  close(fileHandle);
+}
