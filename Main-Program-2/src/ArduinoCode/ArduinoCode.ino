73a9cc3f17b9aa624e5531767dbaa06524afaae1
pi <mattcastle38@gmail.com>
2019-03-25 21:08:12 -0500

    arduino lcd prints hello from main_node

--- Main-Program-2/src/ArduinoCode/ArduinoCode.ino
+++ Main-Program-2/src/ArduinoCode/ArduinoCode.ino
@@ -1,6 +1,8 @@
 #include <ros.h>
 #include <std_msgs/String.h>
-
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 ros::NodeHandle nh;
 
 std_msgs::String str_msg;
@@ -9,6 +11,9 @@ ros::Publisher sender("arduinoPub", &str_msg);
 void receiveCallback(const std_msgs::String& message) {
   str_msg.data = message.data;
   sender.publish(&str_msg);
+  lcd.clear();
+  lcd.print(str_msg.data);
+  
 }
 
 ros::Subscriber<std_msgs::String> receiver("arduinoTopic", &receiveCallback);
@@ -17,6 +22,8 @@ void setup() {
   nh.initNode();
   nh.advertise(sender);
   nh.subscribe(receiver);
+  lcd.begin();
+  lcd.backlight();
 }
 
 void loop() {
