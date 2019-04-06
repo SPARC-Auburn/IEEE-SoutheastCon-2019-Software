dc34666c0381d6d1dae266b1677548d84e530115
Joshua Jablonowski <jdj0019@auburn.edu>
2019-04-06 16:05:02 -0500

    Arduino code for last push
    Dunzo.. for now

--- Test-Programs/potentiometer/potentiometer/potentiometer.ino
+++ Test-Programs/potentiometer/potentiometer/potentiometer.ino
@@ -21,9 +21,9 @@
 
 // These constants won't change.  They're used to give names
 // to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogInPin = A7;  // Analog input pin that the potentiometer is attached to
 const int analogOutPin = 3; // Analog output pin that the LED is attached to
-const int buttonPin = 4;
+const int buttonPin = 2;
 
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
