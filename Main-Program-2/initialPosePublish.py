f316eb1b59635d0a686e792eb0549e6c5989bf89
nmn0005@auburn.edu <noahnedz@live.com>
2019-04-08 19:45:03 -0500

    added initialPosePublisher

---
+++ Main-Program-2/initialPosePublish.py
@@ -0,0 +1,20 @@
+import rospy
+from geometry_msgs import PoseWithCovarianceStamped
+import struct
+def talker():
+    while not rospy.is_shutdown(): 
+        pub = rospy.Publisher('/initialpose', geometry_msgs.msg.PoseWithCovarianceStamped, queue_size=10,latched = True)
+        rospy.init_node('talker', anonymous=True)
+        rate = rospy.Rate(1) # 10hz
+
+        pose = geometry_msgs.msg.PoseWithCovarianceStamped()
+        pose.header.frame_id = "map"
+        pose.pose.pose.position.x=0.1143
+        pose.pose.pose.position.y=0.1143
+        pose.pose.pose.position.z=0
+        pose.pose.covariance=[1e-3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1e-3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1e-3]
+        rospy.loginfo(pose)
+        pub.publish(pose)
+        rospy.spin()
+        rate.sleep()
+    
