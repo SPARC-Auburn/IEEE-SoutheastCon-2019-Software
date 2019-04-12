5df7fc6a82a86bf6114915060097e2b8061188aa
pi <mattcastle38@gmail.com>
2019-04-12 12:33:59 -0500

    sync

--- launches/amcl.sh
+++ launches/amcl.sh
@@ -1,16 +1,16 @@
-roslaunch odometry_publisher odometry_publisher.launch &
+rosrun arduino_talker arduino_talker&
+#roslaunch odometry_publisher odometry_publisher.launch &
 python ../Main-Program-2/odom.py &
 #roslaunch razor-pub.launch &
 roslaunch ydlidar lidar.launch &
 roslaunch ekf_template.launch &
-rosrun map_server map_server ~/mymap/garage.yaml &
+rosrun map_server map_server ~/mymap/practice.yaml &
 sleep 5
 roslaunch test.launch &
-roslaunch ieee_2dnav  move_base.launch &
+#roslaunch ieee_2dnav  move_base.launch &
 sleep 10
 roslaunch amcl_diff.launch &
-rosrun arduino_talker arduino_talker&
 sleep 20
-rosrun main_node main &
+#rosrun main_node main &
 
 
