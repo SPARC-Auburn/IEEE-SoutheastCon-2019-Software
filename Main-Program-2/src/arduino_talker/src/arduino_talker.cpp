ea74b3fe17f2daae3a0d8c72e74864fa1ff2d96e
nmn0005@auburn.edu <noahnedz@live.com>
2019-04-13 11:08:06 -0500

    changed arduino_talker to retardation

--- Main-Program-2/src/arduino_talker/src/arduino_talker.cpp
+++ Main-Program-2/src/arduino_talker/src/arduino_talker.cpp
@@ -14,7 +14,7 @@
 #include <tf/transform_broadcaster.h>
 
 #include "Arduino-Serial/ArduinoSerial.h"
-//Color Indices = red(0), yellow(1), blue(2), green(3)
+
 using namespace std;
 int colorSelect = 0;
 int colorChoose = 0;
@@ -35,18 +35,11 @@ void rin(const std_msgs::Float32ConstPtr &msg){
 void lin(const std_msgs::Float32ConstPtr &msg){
 	leftSpeed = (int)msg->data;
 }
-void colorSelectFunc(const std_msgs::Float32ConstPtr &msg){
-	colorSelect = (int)msg->data;
-}
+
 
 void flagFunc(const std_msgs::Int32 &msg){
 	flagpos = (int)msg->data;
     arduino.moveFlag(flagpos);
-}
-void gateFunc(const std_msgs::Int32 &msg){
-	gatepos = (int)msg->data;
-    arduino.moveFlag(gatepos);
-
 }
 void leftWheelFunc(const std_msgs::Int32 &msg){
     leftWheelVal = msg->data;
@@ -62,59 +55,33 @@ int main(int argc, char **argv){
     ros::Rate loop_rate(40);	//1 Hz
     ros::Subscriber lsub = n.subscribe<std_msgs::Float32>("rmotor_cmd", 1,lin);
     ros::Subscriber rsub = n.subscribe<std_msgs::Float32>("lmotor_cmd", 1,rin);
-    ros::Subscriber flag = n.subscribe<std_msgs::Float32>("flag_cmd", 1,flagFunc);
-    ros::Subscriber gate = n.subscribe<std_msgs::Float32>("gate_cmd", 1,gateFunc);
+    
+    ros::Subscriber flag = n.subscribe<std_msgs::Int32>("flag_cmd", 1,flagFunc);
+   
     ros::Subscriber leftWheel = n .subscribe<std_msgs::Int32>("lwheel",1,leftWheelFunc);
     ros::Subscriber rightWheel = n .subscribe<std_msgs::Int32>("rwheel",1,rightWheelFunc);
 
-    ros::Publisher colorSelectPub = n.advertise<std_msgs::Int32>("colorSelectFunc",1,bool latch = true);
-    ros::Publisher startMatchPub = n.advertise<std_msgs::Int32>("startMatchFunc",1,bool latch = true);
+    
+    ros::Publisher startMatchPub = n.advertise<std_msgs::Int32>("start_match",1,bool latch = true);
     ros::Time current_time = ros::Time::now();
 
     while(ros::ok()) {
         if(matchStatus == 1){
             startMatchPub.publish(matchStatus);
             matchStatus++;
-
         }
 
         std_msgs::String msg;
-	msg.data = std::string("Hello ");
-        if(colorChoose == 0){
-            switch(arduino.getMode()){
-                case 0: arduino.updateLCD("Red");
-                if(arduino.getButtonState() && colorChoose < 1){
-                    colorSelect = arduino.getMode();colorChoose++;}
-                break;
-                case 1: arduino.updateLCD("Yellow");
-                if(arduino.getButtonState() && colorChoose < 1){
-                    colorSelect = arduino.getMode();colorChoose++;}
-                break;
-                case 2: arduino.updateLCD("Blue");
-                if(arduino.getButtonState() && colorChoose < 1){
-                    colorSelect = arduino.getMode();colorChoose++;}
-                break;
-                case 3: arduino.updateLCD("Green");
-                if(arduino.getButtonState() && colorChoose < 1){
-                    colorSelect = arduino.getMode();colorChoose++;}
-                break;
-                default: break;
-            }
-        }
-        if(colorChoose == 1 && matchStatus == 0){
-            colorSelectPub.publish(colorSelect);
-            colorChoose++;   
-        }
-	if(colorChoose > 1 && arduino.getButtonState()){
-		matchStatus = 1;
+	    msg.data = std::string("Hello Fuck Shits");
+       
+        
+	if(arduino.getButtonState()){
+		startMatchPub.publish(1);
 	}
         ros::spinOnce();
-		//cout << "ButtonState " << arduino.getButtonState() << endl;
+		arduino.updateLCD("Fuck");
 		arduino.drive(rightSpeed,leftSpeed);
-        if(leftWheelVal < 4|| rightWheelVal < 4){
-            arduino.updateLCD("L" + atoi(leftWheelVal));
-            arduino.updateLCD("R" + atoi(rightWheelVal));
-        }
+        
 		arduino.updateArduino();
 		loop_rate.sleep();	
     }
