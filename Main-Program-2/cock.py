288ccfff8adfe660c5169a0e58b38bed186e8a12
pi <mattcastle38@gmail.com>
2019-04-13 01:37:23 -0500

    git not working

--- Main-Program-2/cock.py
+++ Main-Program-2/cock.py
@@ -5,6 +5,7 @@ rospy.init_node("NTP20")
 pub = rospy.Publisher('clock', rosgraph_msgs.msg.Clock, queue_size=10)
 msg = rosgraph_msgs.msg.Clock()
 while(1):
-    msg.clock = rospy.Time.from_sec(time.clock())
+    msg.clock = rospy.Time.from_sec(time.time())
         
     pub.publish(msg)
+    time.sleep(0.001)
