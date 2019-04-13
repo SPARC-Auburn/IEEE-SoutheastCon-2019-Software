11408b581c8ceba57009b8ed636932f114df2f96
nmn0005@auburn.edu <noahnedz@live.com>
2019-04-13 01:01:46 -0500

    actually fixed the arduino talker type error

--- Main-Program-2/src/arduino_talker/src/arduino_talker.cpp
+++ Main-Program-2/src/arduino_talker/src/arduino_talker.cpp
@@ -23,6 +23,9 @@ int gatepos = 0;
 int flagpos = 0;
 int rightSpeed = 0;
 int leftSpeed = 0;
+int leftWheelVal = 0;
+int rightWheelVal = 0;
+
 serialPort arduino("/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0");
 
 void rin(const std_msgs::Float32ConstPtr &msg){
@@ -36,14 +39,20 @@ void colorSelectFunc(const std_msgs::Float32ConstPtr &msg){
 	colorSelect = (int)msg->data;
 }
 
-void flagFunc(const std_msgs::Float32ConstPtr &msg){
+void flagFunc(const std_msgs::Int32 &msg){
 	flagpos = (int)msg->data;
     arduino.moveFlag(flagpos);
 }
-void gateFunc(const std_msgs::Float32ConstPtr &msg){
+void gateFunc(const std_msgs::Int32 &msg){
 	gatepos = (int)msg->data;
     arduino.moveFlag(gatepos);
 
+}
+void leftWheelFunc(const std_msgs::Int32 &msg){
+    leftWheelVal = msg->data;
+}
+void rightWheelFunc(const std_msgs::Int32 &msg){
+    rightWheelVal = msg->data;
 }
 
 
@@ -55,13 +64,14 @@ int main(int argc, char **argv){
     ros::Subscriber rsub = n.subscribe<std_msgs::Float32>("lmotor_cmd", 1,rin);
     ros::Subscriber flag = n.subscribe<std_msgs::Float32>("flag_cmd", 1,flagFunc);
     ros::Subscriber gate = n.subscribe<std_msgs::Float32>("gate_cmd", 1,gateFunc);
+    ros::Subscriber leftWheel = n .subscribe<std_msgs::Int32>("lwheel",1,leftWheelFunc);
+    ros::Subscriber rightWheel = n .subscribe<std_msgs::Int32>("rwheel",1,rightWheelFunc);
 
     ros::Publisher colorSelectPub = n.advertise<std_msgs::Int32>("colorSelectFunc",1,true);
     ros::Publisher startMatchPub = n.advertise<std_msgs::Int32>("startMatchFunc",1,true);
     ros::Time current_time = ros::Time::now();
 
     while(ros::ok()) {
-
         if(matchStatus == 1){
             startMatchPub.publish(matchStatus);
             matchStatus++;
@@ -101,6 +111,10 @@ int main(int argc, char **argv){
         ros::spinOnce();
 		//cout << "ButtonState " << arduino.getButtonState() << endl;
 		arduino.drive(rightSpeed,leftSpeed);
+        if(leftWheelVal < 4|| rightWheelVal < 4){
+            arduino.updateLCD("L" + atoi(leftWheelVal));
+            arduino.updateLCD("R" + atoi(rightWheelVal));
+        }
 		arduino.updateArduino();
 		loop_rate.sleep();	
     }
